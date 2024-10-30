739696e5bbb72730495ae05275be80bc53719d56
SireneNouira <sirene.nouira16@gmail.com>
2024-10-30 11:58:45 +0100

    installation ok

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      color:{
+        orange: 'hsl(35,77%,62%)',
+        red: 'hsl(5, 85%, 63%)'
+      }
+    },
+  },
+  plugins: [],
+}
+
